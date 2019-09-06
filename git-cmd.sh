21606754e547d978347e59b72e6f4e1423e1b500
AbdelKRIM DJOUDI <a.djoudi@htcompass.fr>
2019-09-06 16:32:06 +0200

    add file that contains the history of git commands

---
+++ git-cmd.sh
@@ -0,0 +1,67 @@
+    1  git appolo
+    2  git apolo
+    3  cd apolo
+    4  git dev
+    5  git --help
+    6  script.sql.txt git add apolo
+    7  git log
+    8  git anit apolo
+    9  git init apolo
+   10  cd apolo
+   11  clear
+   12  git --help
+   13  git log
+   14  git status
+   15  git tag apolo
+   16  git rm apolo
+   17  cd apolo
+   18  cd ..
+   19  git cleare
+   20  clear
+   21  git commit
+   22  mkdir apolo14
+   23  ls
+   24  git --help
+   25  clear
+   26  git mkdir apolo
+   27  git ls
+   28  ls
+   29  ls -a
+   30  ls -l
+   31  git init
+   32  git add README.md
+   33  touch README.md
+   34  git status
+   35  git commit -m "nada"
+   36  git log
+   37  git add  README.md 
+   38  git status
+   39  git rm --cached README.md 
+   40  git status
+   41  git add  README.md 
+   42  git commit -m "initial commit : add ReadME File"
+   43  git config --global user.name "AbdelKRIM DJOUDI "
+   44  git config --global user.email a.djoudi@htcompass.fr
+   45  touch script-insert-data.sql
+   46  git status
+   47  git add script-insert-data.sql 
+   48  git commit -m "add init script "
+   49  git staus
+   50  git status
+   51  git log 
+   52  git log --online
+   53  git log -online
+   54  git remote add origin https://github.com/htc-abdelkrim/apolo15.git
+   55  git push -u origin master
+   56  clear
+   57  git status
+   58  git commit -a -m "enrich ReadME file" README.md 
+   59  git commit -a -m "enrich ReadME file"
+   60  git diff
+   61  git pull
+   62  git push
+   63  git push
+   64  git pull
+   65  git log
+   66  history
+   67  history > git-cmd.sh
